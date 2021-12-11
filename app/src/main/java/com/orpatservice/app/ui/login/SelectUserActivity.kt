f85072ad1e8525ab372317891d053aa5a60f55af
ajay yadav <>
2021-12-11 20:05:41 +0530

    - Working on AppBarLayout and ToolBar for ActionBar

--- app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
@@ -5,19 +5,21 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import com.orpatservice.app.R
 
-import android.widget.TextView
-
-
-
-
 class SelectUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_user)
 
-        //supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        //supportActionBar?.setCustomView(R.layout.actionbar_layout)
-        //(supportActionBar?.customView?.findViewById(R.id.tvTitle) as TextView).text = getString(R.string.step_1)
+        val ab: ActionBar? = supportActionBar
+
+        ab?.apply {
+            title = "Toolbar Back Button Example"
+
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
 
     }
 }
