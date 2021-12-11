597caaeccf6ab709029164fdaeabc31b1146f944
manali <manali@esoftech.com>
2021-12-11 15:50:50 +0530

    Dashboard design

--- app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
@@ -1,12 +1,28 @@
 package com.orpatservice.app.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.orpatservice.app.R
+import com.orpatservice.app.ui.dashboard.DashboardActivity
 
 class SelectUserActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_user)
+
+        findViewById<TextView>(R.id.tv_heading).setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    DashboardActivity::class.java
+                )
+            )
+        }
+
     }
+
+
 }
