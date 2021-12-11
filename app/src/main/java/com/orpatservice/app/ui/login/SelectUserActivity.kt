5d6898431f0f8bc66e4e279dde1e182f9a3fbb39
ajay yadav <>
2021-12-11 20:15:10 +0530

    - Working on AppBarLayout and ToolBar for ActionBar - Gradle upgrade

--- app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
@@ -2,19 +2,18 @@ package com.orpatservice.app.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import com.orpatservice.app.R
+import kotlinx.android.synthetic.main.activity_select_user.*
 
 class SelectUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_user)
 
-        val ab: ActionBar? = supportActionBar
+        // set toolbar as support action bar
+        setSupportActionBar(toolbar)
 
-        ab?.apply {
-            title = "Toolbar Back Button Example"
-
+        supportActionBar?.apply {
             // show back button on toolbar
             // on back button press, it will navigate to parent activity
             setDisplayHomeAsUpEnabled(true)
