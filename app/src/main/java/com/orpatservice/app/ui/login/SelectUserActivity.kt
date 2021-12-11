feae424f4786dad7cfaeca70ba0e49efc2cec58e
ajay yadav <>
2021-12-11 15:58:29 +0530

    - Working on Action bar

--- app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
@@ -2,11 +2,22 @@ package com.orpatservice.app.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.orpatservice.app.R
+
+import android.widget.TextView
+
+
+
 
 class SelectUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_user)
+
+        //supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        //supportActionBar?.setCustomView(R.layout.actionbar_layout)
+        //(supportActionBar?.customView?.findViewById(R.id.tvTitle) as TextView).text = getString(R.string.step_1)
+
     }
 }
