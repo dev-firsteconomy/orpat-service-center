04b41e0c4fc7b5083da0b9276dfbac2e1a5414cb
ajay yadav <>
2021-12-11 21:54:47 +0530

    - Enter Mobile Number Screen Done - Code optimization

--- app/src/main/java/com/orpatservice/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/login/LoginActivity.kt
@@ -3,10 +3,22 @@ package com.orpatservice.app.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.orpatservice.app.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // set toolbar as support action bar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            title = ""
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
     }
 }
--- app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/login/SelectUserActivity.kt
@@ -1,7 +1,9 @@
 package com.orpatservice.app.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.orpatservice.app.R
 import kotlinx.android.synthetic.main.activity_select_user.*
 
@@ -14,11 +16,16 @@ class SelectUserActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.apply {
+            title = ""
             // show back button on toolbar
             // on back button press, it will navigate to parent activity
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+    }
 
+    fun signUp(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
