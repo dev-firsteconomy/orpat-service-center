45f4cefff5bf51f860ee220d2e1692b3d7477443
manali <manali@esoftech.com>
2021-12-24 21:09:05 +0530

    --refactor customer details

--- app/src/main/java/com/orpatservice/app/ui/request/CustomerDetailsActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/request/CustomerDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.orpatservice.app.ui.request
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.orpatservice.app.R
 import com.orpatservice.app.databinding.ActivityCustomerDetailsBinding
 import com.orpatservice.app.databinding.ActivityTechniciansBinding
@@ -12,6 +13,25 @@ class CustomerDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCustomerDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        supportActionBar?.apply {
+            title = ""
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
