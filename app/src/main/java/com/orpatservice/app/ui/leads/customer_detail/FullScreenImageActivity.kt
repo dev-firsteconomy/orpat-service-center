f807b66072e08ca4ef0b1ce36e1688516b5c7fda
dev.vikas2020 <Android.vikas@750>
2022-01-02 21:13:47 +0530

    --Create fullscreen activity

---
+++ app/src/main/java/com/orpatservice/app/ui/leads/customer_detail/FullScreenImageActivity.kt
@@ -0,0 +1,12 @@
+package com.orpatservice.app.ui.leads.customer_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.orpatservice.app.R
+
+class FullScreenImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_image)
+    }
+}
