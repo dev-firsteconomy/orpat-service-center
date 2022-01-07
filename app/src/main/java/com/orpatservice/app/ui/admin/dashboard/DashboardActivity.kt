03f5cfd28d2df1c743530e0e8c76ec0f0c9e40b8
ajay yadav <>
2022-01-07 17:16:11 +0530

    - minor change

--- app/src/main/java/com/orpatservice/app/ui/admin/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/orpatservice/app/ui/admin/dashboard/DashboardActivity.kt
@@ -15,7 +15,6 @@ import com.orpatservice.app.ui.leads.service_center.LeadHistoryActivity
 import com.orpatservice.app.ui.leads.service_center.RequestLeadActivity
 import com.orpatservice.app.utils.Constants
 import com.orpatservice.app.data.sharedprefs.SharedPrefs
-import com.orpatservice.app.ui.technician.HappyCodeActivity
 import com.orpatservice.app.ui.leads.technician.TechnicianHistoryLeadActivity
 import com.orpatservice.app.ui.login.SelectUserActivity
 import com.orpatservice.app.ui.leads.technician.TechnicianRequestLeadActivity
@@ -107,7 +106,6 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(Intent(this, ProfileActivity::class.java))
             }
             R.id.mcv_payment -> {
-                startActivity(Intent(this, HappyCodeActivity::class.java))
             }
             R.id.mcv_more -> {
 
